9311d1463c622f274ef4b592aebd5d9074ef34ff
10tekinvo@gmail.com <10tekinvo@gmail.com>
2020-01-14 19:35:31 +0600

    Add project files.

---
+++ FTPClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace FTPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var request = (FtpWebRequest)WebRequest.Create(new Uri("ftp://127.0.0.1"));
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            //request.Credentials = new NetworkCredential("admin", "123123");
+            //request.EnableSsl = true; //- используется ssl(ftps://)
+            request.UseBinary = false;
+
+            var response = (FtpWebResponse)request.GetResponse();
+
+            using (var stream = response.GetResponseStream())
+            {
+                File.OpenWrite()
+            }
+        }
+    }
+}
